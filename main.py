df4d27c6b920b64cde873b00cdcb67532067632c
Russell McNally <russmcn85@gmail.com>
2024-05-09 09:17:04 +0100

    Read text file and report word count

---
+++ main.py
@@ -0,0 +1,17 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+
+def get_num_words(words):
+    return len(words.split())
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
